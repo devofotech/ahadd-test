fc0886ff9a11eceead4454e6b3fb008d3e581c78
Naqib Zulkifli <nqbzlkfl@gmail.com>
2022-08-18 07:11:06 +0000

    Login loading redirect

---
+++ src/@Pages/Auth/LandingPageAhadd/Redirect.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+import { Grid } from '@material-ui/core';
+import plus_ahadd from '@Assets/Images/plus_ahadd.svg';
+import '@Components/CenteredLoading/text.css';
+import './index.css';
+
+export default function LoginForm() {
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('@Assets/Images/driving-lottie.json'),
+    });
+  }, []);
+
+  return (
+    <Grid container alignItems="center" justify="center" className="d-flex flex-column" style={{ height: '100vh' }}>
+      <img
+        src={plus_ahadd}
+        className="img-fluid fade-in"
+        style={{ width: '28rem', paddingBottom: '0.75rem' }}
+      />
+      <div className="fade-in my-3" ref={container} style={{ height: 350, marginBottom: 50 }} />
+      <h3 className="loading-text fade-in">Please wait, try to login you</h3>
+    </Grid>
+  );
+}
---
+++ src/@Pages/Auth/LandingPageAhadd/UnauthorizedLogin.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import {
+  Dialog, DialogActions, DialogContent, DialogContentText
+} from '@material-ui/core';
+import Button from '@Components/Button';
+
+export default function UnauthorizedLogin(h) {
+  const handleClose = () => h.setError(false);
+  return (
+    <Dialog open={h.error} onClose={handleClose}>
+      <DialogContent>
+        <Typography ariant="h6" align="center" className="mb-4" style={{ fontWeight: 600, color: 'red' }}>
+          There was an error to connect your account with Microsoft
+        </Typography>
+        <DialogContentText align="center">
+          Please try again in a few minutes.
+        </DialogContentText>
+        <DialogContentText align="center" className="mt-1">
+          If the problem does not solve, please contact your team.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions className={'mx-2 mb-2 d-flex \'justify-content-end\'}'}>
+        <Button onClick={handleClose} className="color-gradient-inline" style={{ width: '8rem' }}>
+          CLOSE
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
