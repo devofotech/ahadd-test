1cbe534b6944db1dcd5ee3cf4f510f9670d160ba
Naqib Zulkifli <73631168+nqbzlkfl@users.noreply.github.com>
2022-08-18 13:31:06 +0800

    Topbar Ui

---
+++ src/@Assets/Icons/topbarIcon.js
@@ -0,0 +1,61 @@
+/* eslint-disable max-lines */
+/* eslint-disable max-len */
+import { primaryColor } from '@Assets/css';
+
+export const Dashboard = ({ color = '#8b95ab' }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+      <g id="Icon_material-dashboard" data-name="Icon material-dashboard" transform="translate(-4.5 -4.5)" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M4.5,13.389h7.111V4.5H4.5Zm0,7.111h7.111V15.167H4.5Zm8.889,0H20.5V11.611H13.389Zm0-16V9.833H20.5V4.5Z" stroke="none" />
+        <path d="M 19 19 L 19 13.11110973358154 L 14.88889026641846 13.11110973358154 L 14.88889026641846 19 L 19 19 M 10.11110973358154 19 L 10.11110973358154 16.66666984558105 L 6 16.66666984558105 L 6 19 L 10.11110973358154 19 M 10.11110973358154 11.88889026641846 L 10.11110973358154 6 L 6 6 L 6 11.88889026641846 L 10.11110973358154 11.88889026641846 M 19 8.333330154418945 L 19 6 L 14.88889026641846 6 L 14.88889026641846 8.333330154418945 L 19 8.333330154418945 M 20.5 20.5 L 13.38889026641846 20.5 L 13.38889026641846 11.61110973358154 L 20.5 11.61110973358154 L 20.5 20.5 Z M 11.61110973358154 20.5 L 4.5 20.5 L 4.5 15.16666984558105 L 11.61110973358154 15.16666984558105 L 11.61110973358154 20.5 Z M 11.61110973358154 13.38889026641846 L 4.5 13.38889026641846 L 4.5 4.5 L 11.61110973358154 4.5 L 11.61110973358154 13.38889026641846 Z M 20.5 9.833330154418945 L 13.38889026641846 9.833330154418945 L 13.38889026641846 4.5 L 20.5 4.5 L 20.5 9.833330154418945 Z" stroke="none" fill={color} />
+      </g>
+    </svg>
+  );
+};
+
+export const MapView = ({ color = '#8b95ab' }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20.1" height="17.5" viewBox="0 0 20.1 17.5">
+      <g id="Icon_feather-map" data-name="Icon feather-map" transform="translate(-0.75 -2.25)">
+        <path id="Path_2114" data-name="Path 2114" d="M1.5,6.2V19l5.918-3.2L14.182,19,20.1,15.8V3L14.182,6.2,7.418,3Z" transform="translate(0 0)" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+        <path id="Path_2115" data-name="Path 2115" d="M12,3V15.8" transform="translate(-4.582 0)" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+        <path id="Path_2116" data-name="Path 2116" d="M24,9V21.8" transform="translate(-9.818 -2.8)" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+      </g>
+    </svg>
+  );
+};
+
+export const AssetList = ({ color = '#8b95ab' }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="15.896" height="17.496" viewBox="0 0 15.896 17.496">
+      <g id="Icon_feather-home" data-name="Icon feather-home" transform="translate(-3.75 -2.25)">
+        <path id="Path_2112" data-name="Path 2112" d="M4.5,8.6,11.7,3l7.2,5.6v8.8A1.6,1.6,0,0,1,17.3,19H6.1a1.6,1.6,0,0,1-1.6-1.6Z" transform="translate(0 0)" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+        <path id="Path_2113" data-name="Path 2113" d="M13.5,26V18h4.8v8" transform="translate(-4.201 -7.002)" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+      </g>
+    </svg>
+  );
+};
+
+export const Analytic = ({ color = '#8b95ab' }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="17.539" height="17.53" viewBox="0 0 17.539 17.53">
+      <g id="Icon_feather-pie-chart" data-name="Icon feather-pie-chart" transform="translate(-2.212 -2.25)">
+        <path id="Path_2117" data-name="Path 2117" d="M18.369,14.694A8,8,0,1,1,7.8,4.245" transform="translate(0 -0.581)" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+        <path id="Path_2118" data-name="Path 2118" d="M26,11a8,8,0,0,0-8-8v8Z" transform="translate(-7 0)" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+      </g>
+    </svg>
+  );
+};
+
+export const AboutUs = ({ color = '#8b95ab' }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16.494" height="16.49" viewBox="0 0 17.494 19.758">
+      <g id="Layer_2" data-name="Layer 2" transform="translate(0.244 0.254)">
+        <g id="Layer_1" data-name="Layer 1">
+          <path id="Path_2147" data-name="Path 2147" d="M15.24.65h0c-.77.45-1.54.9-2.29,1.38A42.44,42.44,0,0,0,7.32,6.22a13.76,13.76,0,0,0-2.5,2.83A4.56,4.56,0,0,0,4,11.21a3,3,0,0,0,1.6,2.84,7.79,7.79,0,0,0,2.7.88,19.12,19.12,0,0,0,3.3.2h2.48c-.13.42-.26.82-.39,1.22-.24.74-.47,1.48-.71,2.23a.19.19,0,0,1-.22.17h-4a16.5,16.5,0,0,1-4.41-.64,6.38,6.38,0,0,1-2.81-1.6,3.89,3.89,0,0,1-1-3.11A7.66,7.66,0,0,1,2.65,8.75,24.29,24.29,0,0,1,6.4,5.48,49.08,49.08,0,0,1,15.12.66h.11Z" fill="rgba(0,0,0,0)" stroke={color} strokeLinejoin="round" strokeWidth="1.5" />
+          <path id="Path_2148" data-name="Path 2148" d="M16.5,8.79a13,13,0,0,0-1.57,3.83c-.06.19-.14.23-.33.23H9.81a5.26,5.26,0,0,1-2.24-.49A2,2,0,0,1,6.4,9.71,7.25,7.25,0,0,1,7.73,7.37,29.15,29.15,0,0,1,12,3.15c1.07-.87,2.19-1.69,3.33-2.47.08-.06.13-.17.24-.18h0a9,9,0,0,1-.84.8c-1.1,1-2.21,2-3.24,3.1A12.52,12.52,0,0,0,9.65,6.8a3,3,0,0,0-.28.63.93.93,0,0,0,.8,1.31,3.35,3.35,0,0,0,.63,0h5.7Z" fill="rgba(0,0,0,0)" stroke={color} strokeLinejoin="round" strokeWidth="1.5" />
+        </g>
+      </g>
+    </svg>
+  );
+};
